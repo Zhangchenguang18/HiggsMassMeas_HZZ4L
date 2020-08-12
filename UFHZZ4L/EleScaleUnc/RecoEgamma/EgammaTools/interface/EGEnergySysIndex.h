a74d213be8d04206ca6c130f4e81cd458134c6d4
chenguang <zhangcg@ihep.ac.cn>
2020-08-12 18:12:41 -0400

    specail function for UFHZZ4L framework

---
+++ UFHZZ4L/EleScaleUnc/RecoEgamma/EgammaTools/interface/EGEnergySysIndex.h
@@ -0,0 +1,56 @@
+#ifndef RecoEgamma_EgammaTools_EGEnergySysIndex_h
+#define RecoEgamma_EgammaTools_EGEnergySysIndex_h
+
+//Currently the Electron and Photon energy calibrators return a big vector of calculated
+//energy and systematic values
+//this class allows the elements to be indexed easier
+//ultimately should become a class
+
+#include <array>
+
+class EGEnergySysIndex {
+public:
+  enum Index {
+    kScaleStatUp=0,
+    kScaleStatDown,
+    kScaleSystUp,
+    kScaleSystDown,
+    kScaleGainUp,
+    kScaleGainDown,
+    kSmearRhoUp,
+    kSmearRhoDown,
+    kSmearPhiUp,
+    kSmearPhiDown,
+    kScaleUp,
+    kScaleDown,
+    kSmearUp,
+    kSmearDown,
+    kScaleValue,
+    kSmearValue,
+    kSmearNrSigma,
+    kEcalPreCorr,
+    kEcalErrPreCorr,
+    kEcalPostCorr,
+    kEcalErrPostCorr,
+    kEcalTrkPreCorr,
+    kEcalTrkErrPreCorr,
+    kEcalTrkPostCorr,
+    kEcalTrkErrPostCorr,
+    gainseed,
+    r9,
+    scabseta,
+    ScaleUpECALOnly,
+    ScaleDnECALOnly
+  };
+  static constexpr size_t kNrSysErrs=ScaleDnECALOnly+1; 
+
+  static const std::string& name(size_t index){return names_[index];}
+
+private:
+  static const std::array<std::string,kNrSysErrs> names_;
+
+
+   
+};
+
+#endif
